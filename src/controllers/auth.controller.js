7aa3f249634b6ec79bc5b90a66c9673b09b75543
expo <expoworld112@gmail.com>
2026-02-14 11:42:31 +0530

    auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,6 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Password must be at least 6 characters" });
     }
 
-    // check if emailis valid: regex
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Invalid email format" });
@@ -27,7 +26,6 @@ export const signup = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "Email already exists" });
 
-    // 123456 => $dnjasdkasj_?dmsakmk
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -38,12 +36,7 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // before CR:
-      // generateToken(newUser._id, res);
-      // await newUser.save();
-
-      // after CR:
-      // Persist user first, then issue auth cookie
+      
       const savedUser = await newUser.save();
       generateToken(savedUser._id, res);
 
@@ -112,27 +105,8 @@ export const logout = (_, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 };
 
-// export const updateProfile = async (req, res) => {
-//   try {
-//     const { profilePic } = req.body;
-//     if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
 
-//     const userId = req.user._id;
 
-//     const uploadResponse = await cloudinary.uploader.upload(profilePic);
-
-//     const updatedUser = await User.findByIdAndUpdate(
-//       userId,
-//       { profilePic: uploadResponse.secure_url },
-//       { new: true }
-//     );
-
-//     res.status(200).json(updatedUser);
-//   } catch (error) {
-//     console.log("Error in update profile:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
  export const updateProfile = async (req, res) => {
   try {
     if (!req.file) {
