b183c4198302f0c8571f77a1b88481f36ed0fa3f
expo <expoworld112@gmail.com>
2026-02-16 13:59:50 +0530

    arject

--- src/lib/arcjet.js
+++ src/lib/arcjet.js
@@ -1,39 +1,22 @@
 
-// import { ENV } from "./env.js";
-// import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
-
-// const aj = arcjet({
-//   key: ENV.ARCJET_KEY,
-//   rules: [
-//     shield({ mode: "LIVE" }),
-//     detectBot({
-//       mode: "LIVE",
-//       allow: ["CATEGORY:SEARCH_ENGINE"],
-//     }),
-//     tokenBucket({
-//       mode: "LIVE",
-//       refillRate: 5,
-//       interval: 10,
-//       capacity: 10,
-//     }),
-//   ],
-// });
-
-// export default aj;
 import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
 import { ENV } from "./env.js";
 
 export const aj = arcjet({
   key: ENV.ARCJET_KEY,
 
-  characteristics: ["ip.src"],
+  characteristics: ["ip.src" , "http.request.headers.user-agent"],
 
   rules: [
     shield({ mode: "LIVE" }),
 
     detectBot({
       mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      allow: ["CATEGORY:SEARCH_ENGINE",
+                "CATEGORY:PREVIEW",
+        "CATEGORY:MONITOR",
+
+      ],
     }),
 
     tokenBucket({
